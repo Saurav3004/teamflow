a0627734b3038315df996c2abdae467b17409582
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-10-14 20:25:37 +0530

    Authentication and billing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,15 @@
+import { HeroHeader } from "@/app/_components/header";
+import HeroSection from "@/app/_components/hero-section";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    <ThemeToggle />
+    
+    < HeroHeader />
+
+    <HeroSection />
    </div>
   );
 }
